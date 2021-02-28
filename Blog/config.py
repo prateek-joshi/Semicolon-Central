21f766d9bb636b908a8068b1168c19f39fe72070
Prateek Joshi <pjoshi22900@gmail.com>
2021-02-28 20:09:11 +0530

    Added environment variables

--- Blog/config.py
+++ Blog/config.py
@@ -2,9 +2,7 @@ import os
 
 class Config:
 	SQLALCHEMY_DATABASE_URI = os.environ.get('SC_DB_URI')
-	# SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
 	SECRET_KEY = os.environ.get('SC_SECRET_KEY')
-	# SECRET_KEY = '93fcca7d58d595b07316b21e744b38af'
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
